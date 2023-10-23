615ea5dc1681d284f1cf9d5e06d303047bd35077
robsonnsbr <robsonnsbr@gmail.com>
2023-10-23 13:58:44 -0300

    fix: include version

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,11 @@ function App() {
     <>
       <AppRoutes />
       <GlobalStyle />
+      <span
+        style={{ fontSize: "12px", position: "absolute", bottom: 0, left: 0 }}
+      >
+        <i>0.0.0v under development</i>
+      </span>
     </>
   );
 }
